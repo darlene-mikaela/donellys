aff246bc582ebe1c9dd3aa56f2ee8cee80bd9342
darlenemikaela <darlenemikaela.id@gmail.com>
2026-05-30 15:24:55 +0800

    changed designs and added checkout success pop up

---
+++ src/components/CheckoutSuccess.jsx
@@ -0,0 +1,11 @@
+export default function CheckoutSuccess() {
+    return (
+        <div className="checkout-success-overlay">
+            <div className="checkout-success-content">
+                <div className="big-check">&#x2714;</div>
+                <h2>Checkout success</h2>
+                <p>Thank you for your order. Please wait for us to confirm your order.</p>
+            </div>
+        </div>
+    )
+}
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -6,7 +6,10 @@ import Footer from "../components/Footer";
 export default function Cart({ basket, onUpdateQuantity, onDeleteClick, onTrashClick }) {
     const [showAddress, setShowAddress] = useState(false);
     // calculate price
-    const totalPrice = basket ? basket.reduce((item) => (item.price * item.quantity), 0) : 0;
+    const totalPrice = basket ? basket.reduce((sum, item) => sum + (item.price * item.quantity), 0) : 0;
+    const today = new Date(); // todays date
+    today.setDate(today.getDate() + 1);
+    const minDate = today.toISOString().split('T')[0];
 
     if (basket.length === 0) {
         return (
@@ -24,65 +27,99 @@ export default function Cart({ basket, onUpdateQuantity, onDeleteClick, onTrashC
     return (
         <div className="cart-wrapper">
             <SiteHeader />
+            <h1>Checkout</h1>
             <div className="cart-container">
-                <h1>Checkout</h1>
-                {basket.map((item) => (
-                    <CartCards key={`${item.id}-${item.size}`}
-                        item={item}
-                        onUpdateQuantity={onUpdateQuantity}
-                        onDeleteClick={onDeleteClick}
-                        onTrashClick={onTrashClick} />
-                ))}
-            </div>
-            <hr className="summary-divider" />
-            <div className="total-row">
-                <span>Total amount: </span>
-                <span className="grand-total">Rp{totalPrice}</span>
-            </div>
-
-            <div className="shipping-form-container">
-                <h3>Shipping Details</h3>
-                <p>Please fill out your delivery details to complete your order.</p>
-                <form>
-                    <div className="personal-info">
-                        <label htmlFor="sender-name">Full Name</label>
-                        <input type="text" name="sender-name" id="sender-name" placeholder="John Doe" required />
-
-                        <label htmlFor="phone-number">Whatsapp/Phone Number</label>
-                        <input type="number" name="phone-number" id="phone-number" placeholder="081234567890" required max={999999999999} />
+                <div className="cart-item-container">
+                    {basket.map((item) => (
+                        <CartCards key={`${item.id}-${item.size}`}
+                            item={item}
+                            onUpdateQuantity={onUpdateQuantity}
+                            onDeleteClick={onDeleteClick}
+                            onTrashClick={onTrashClick} />
+                    ))}
+                    <div className="total-row">
+                        <span className="grand-total">Total amount: Rp{totalPrice.toLocaleString()}</span>
+                        <p>* Does not include shipping fees.</p>
                     </div>
+                </div>
 
-                    <div className="send-method">
-                        <p>Delivery method</p>
-                        <input type="radio" name="send-method" id="delivery" onClick={()=>setShowAddress(true)}/>
-                        <label htmlFor="delivery">Home Delivery</label><br />
-
-                        <input type="radio" name="send-method" id="pickup" onClick={()=>setShowAddress(false)}/>
-                        <label htmlFor="pickup">Pick up</label>
+                <div className="shipping-form-container">
+                    <div className="shipping-title">
+                        <h3>Shipping Details</h3>
+                        <p>Please fill out your delivery details to complete your order.</p>
                     </div>
+                    <form>
+                        <h4>Personal Info</h4>
+                        <div className="personal-info">
+                            <div className="sender-name-container">
+                                <label htmlFor="sender-name">Sender's Full Name</label>
+                                <input type="text" name="sender-name" id="sender-name" placeholder="John Doe" required /><br />
+                            </div>
 
-                    {showAddress && <>
-                        <div className="delivery-address">
-                            <label htmlFor="city">City/Region</label>
-                            <input type="text" name="city" id="city" required placeholder="Surabaya" />
-
-                            <label htmlFor="full-address">Full Address</label>
-                            <textarea name="full-address" id="full-address" rows={4} cols={50} />
-
-                            <label htmlFor="post-code">Postal/Zip Code</label>
-                            <input type="text" name="post-code" id="post-code" />
+                            <div className="phone-number-container">
+                                <label htmlFor="phone-number">Whatsapp/Phone Number</label>
+                                <input type="text" name="phone-number" id="phone-number"
+                                    placeholder="081234567890" required inputMode="numeric"
+                                    pattern="0[0-9]{10,13}" />
+                            </div>
                         </div>
-                    </>}
 
-                    <div className="schedule">
-                        <label htmlFor="delivery-date">Delivery Date</label>
-                        <input type="date" name="delivery-date" id="delivery-date" />
+                        <h4>Delivery Method</h4>
+                        <div className="send-method">
+                            <div className="container">
+                                <input type="radio" name="send-method" id="delivery" onClick={() => setShowAddress(true)} />
+                                <label htmlFor="delivery">Home Delivery</label>
+                            </div>
+                            <div className="container">
+                                <input type="radio" name="send-method" id="pickup" onClick={() => setShowAddress(false)} defaultChecked />
+                                <label htmlFor="pickup">Pick up</label>
+                            </div>
+                        </div>
 
-                        <label htmlFor="notes">Special Requests</label>
-                        <textarea name="notes" id="notes"></textarea>
-                    </div>
+                        {showAddress && <>
+                            <div className="delivery-address">
+                                <div className="container">
+                                    <label htmlFor="city">City/Region</label>
+                                    <input type="text" name="city" id="city" required placeholder="Surabaya" />
+                                </div>
 
-                </form>
+                                <div className="container">
+                                    <label htmlFor="full-address">Full Address</label>
+                                    <textarea name="full-address" id="full-address" rows={4} cols={30} required />
+                                </div>
+
+                                <div className="container">
+                                    <label htmlFor="post-code">Postal/Zip Code</label>
+                                    <input type="number" name="post-code" id="post-code" required inputMode="numeric"
+                                        pattern="[0-9]{5}" />
+                                </div>
+                            </div>
+                        </>}
+
+                        <h4>Delivery/Pick up Date and Additional Notes</h4>
+                        <div className="schedule">
+                            <div className="container">
+                                <label htmlFor="delivery-date">Delivery Date</label>
+                                <input type="date" name="delivery-date" id="delivery-date" min={minDate} required />
+                            </div>
+
+                            <div className="container">
+                                <label htmlFor="pickup-time">Pickup/Deliver Time</label>
+                                <input type="time" name="time" id="pickup-time" required />
+                            </div>
+
+                            <div className="container">
+                                <label htmlFor="notes">Special Requests</label>
+                                <textarea name="notes" id="notes" rows={4} cols={30}
+                                    placeholder="A Christmas card; put the order on the fence; etc."></textarea>
+                            </div>
+                        </div>
+
+                        <div className="submit-btn">
+                            <button name="submit">Finish my order!</button>
+                        </div>
+                    </form>
+                </div>
             </div>
             <Footer />
         </div>
