8a168164b16d1503b7f39cb2f89a5b25d75722f5
darlenemikaela <darlenemikaela.id@gmail.com>
2026-05-31 20:36:30 +0800

    toggled design

--- src/components/CartCards.jsx
+++ src/components/CartCards.jsx
@@ -3,13 +3,15 @@ import React, { useState } from "react";
 export default function CartCards({ item, onUpdateQuantity, onDeleteClick, onTrashClick }) {
     return (
         <div className="cart-card">
-            <div className="img-container-cart">
-                <img src={item.image} alt={item.title} />
-            </div>
-            <div className="cart-item-details">
-                <h3>{item.title} ({item.size})</h3>
-                <p>Qty: {item.quantity} (@Rp{item.price.toLocaleString()})</p>
-                <p>Rp{(item.quantity * item.price).toLocaleString()}</p>
+            <div className="cart-main-info">
+                <div className="img-container-cart">
+                    <img src={item.image} alt={item.title} />
+                </div>
+                <div className="cart-item-details">
+                    <h3>{item.title} ({item.size})</h3>
+                    <p>Qty: {item.quantity} (@Rp{item.price.toLocaleString()})</p>
+                    <p>Rp{(item.quantity * item.price).toLocaleString()}</p>
+                </div>
             </div>
             <div className="cart-actions">
                 <div className="counter-container">
